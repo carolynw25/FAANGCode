4312c18e0d55529e44ba18ea74e03e6d28e3c422
Sam Chen <76010610+SamchenUF@users.noreply.github.com>
2025-02-17 19:31:32 -0500

    Created popup.html and linked popup to api call, separated website and popup

--- faang_code/public/background/background.js
+++ faang_code/public/background/background.js
@@ -38,7 +38,8 @@ chrome.runtime.onConnect.addListener((port) => {
         if (message.action === "sendProblemData") {
             try {
                 const prompt = await callGPTApi({
-                    prompt: `You are an expert programmer. You will be given a coding problem and a solution to the problem. Your task is to analyze the code and determine if it is correct or not. If the code is correct, respond with "CORRECT". If the code is incorrect, respond with "INCORRECT" and provide a brief explanation of the error(s) in the code. The coding problem is as follows: ${message.problemDescription} and this is my code: ${message.problemCode}`
+                    prompt: 'Hello'
+                    //prompt: `You are an expert programmer. You will be given a coding problem and a solution to the problem. Your task is to analyze the code and determine if it is correct or not. If the code is correct, respond with "CORRECT". If the code is incorrect, respond with "INCORRECT" and provide a brief explanation of the error(s) in the code. The coding problem is as follows: ${message.problemDescription} and this is my code: ${message.problemCode}`
                 });
 
                 port.postMessage({
