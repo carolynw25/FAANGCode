33658acb208a674b182768d8c392500bb9f8421b
erink <Ehargrave@ufl.edu>
2025-02-27 16:59:28 -0500

    brainstorming for Problems page

--- faang_code/src/Pages/Profile/index.js
+++ faang_code/src/Pages/Profile/index.js
@@ -3,39 +3,41 @@ import './index.css';
 
 function Profile() {
     return (
-        <div>
+        <div className="form-container">
             <h1>Profile</h1>
             {/* display firstname, lastname, username, email, password, etc */}
-            <div>
+            <div className="form-field">
                 <label>
                     First Name:
                 </label>
+
+
             </div>
 
-            <div>
+            <div className="form-field">
                 <label>
                     Last Name:
                 </label>
             </div>
 
-            <div>
+            <div className="form-field">
                 <label>
                     Email:
                 </label>
             </div>
 
-            <div>
+            <div className="form-field">
                 <label>
                     User Name:
                 </label>
             </div>
 
-            <div>
+            <div className="form-field">
                 <label>
                     Password:
                 </label>
             </div>
-    </div>
+        </div>
     );
 
 }
